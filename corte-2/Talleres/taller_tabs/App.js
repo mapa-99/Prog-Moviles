1ffdd20725d8a1fde86d864a76fc32a1c233e00a
Manuel Paredes <mapa991@hotmail.com>
2022-10-25 10:43:09 -0500

    Creación carpeta Proyecto final. Taller #11, tabs

---
+++ corte-2/Talleres/taller_tabs/App.js
@@ -0,0 +1,21 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/pages/HomeScreen';
+import SettingsScreen from './src/pages/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return ( 
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
