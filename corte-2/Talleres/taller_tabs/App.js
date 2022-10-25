a2d1beead298235fa7c4e47991b29b293f760661
Manuel Paredes <mapa991@hotmail.com>
2022-10-25 12:08:41 -0500

    Iconos agregados y nueva pantalla perfil

--- corte-2/Talleres/taller_tabs/App.js
+++ corte-2/Talleres/taller_tabs/App.js
@@ -4,14 +4,33 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/pages/HomeScreen';
 import SettingsScreen from './src/pages/SettingsScreen';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import ProfileScreen from './src/pages/ProfileScreen';
 const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return ( 
+  return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Settings') {
+              iconName = 'sliders';
+            } else if (route.name === 'Profile') {
+              iconName = 'user';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
--- corte-2/Talleres/taller_tabs/src/pages/ProfileScreen.jsx
+++ corte-2/Talleres/taller_tabs/src/pages/ProfileScreen.jsx
@@ -1,12 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const HomeScreen = () => {
+const ProfileScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>This is the Home Screen :)!</Text>
+      <Text>This is the Profile Screen :) !</Text>
     </View>
   );
 };
 
-export default HomeScreen;
+export default ProfileScreen;
